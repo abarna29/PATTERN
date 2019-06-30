12894956db9b91a40f8410f8988d9214ed82b388
abarna29 <47861230+abarna29@users.noreply.github.com>
2019-06-30 12:52:29 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -24,3 +24,31 @@ int main()
 	}
 	return 0;
 }
+
+
+OUTPUT
+  1   2   3   4 
+  5   6   7   8 
+  9  10  11  12 
+ 13  14  15  16 
+ 17  18  19  20 
+
+
+#include <stdio.h>
+
+int main() 
+{
+	int n,m;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	int c=0;
+	for(int i=0;i<n;i++)
+	{
+	    for(int j=0;j<m;j++)
+	    {
+	        printf("%3d ",++c);
+	    }
+	    printf("\n");
+	}
+	return 0;
+}
